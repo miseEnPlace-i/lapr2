06a837184c0eddcfbe9260990a6f20d97317f491
Tomás Russo <tomas.russo4@gmail.com>
2022-04-29 12:00:43 +0100

    feat(us-10): added RegisterEmployeeController and CreateEmployeeUI

---
+++ src/main/java/app/controller/RegisterEmployeeController.java
@@ -0,0 +1,63 @@
+package app.controller;
+
+import app.domain.model.Company;
+import app.domain.model.Employee;
+import app.domain.model.store.EmployeeStore;
+
+/**
+ * Register SNS User Controller
+ * 
+ * @author Tomás Russo <1211288@isep.ipp.pt>
+ */
+
+public class RegisterEmployeeController {
+    private App app;
+    private Company company;
+    private Employee employee;
+    private EmployeeStore store;
+
+    /**
+     * Constructor for RegisterEmployeeController.
+     */
+    public RegisterEmployeeController() {
+        this.app = App.getInstance();
+        this.company = this.app.getCompany();
+        this.store = this.company.getEmployeeStore();
+        this.employee = null;
+    }
+
+    /**
+     * Creates an Employee instance and validates it.
+     * 
+     * @param name the employee name
+     * @param address the employee address
+     * @param phoneNumber the employee phoneNumber
+     * @param email the employee email
+     * @param citizenCardNumber the employee citizenCardNumber
+     * @param roleId the employee roleId
+     */
+    public void createEmployee(String name, String address, String phoneNumber, String email, String citizenCardNumber, String roleId) {
+        // create an instance of an Employee
+        this.employee = store.createEmployee(name, address, phoneNumber, email, citizenCardNumber, roleId);
+
+        // validate the Employee
+        store.validateEmployee(employee);
+    }
+
+    /**
+     * Adds an Employee to the Employee store.
+     */
+    public void saveEmployee() {
+        store.saveEmployee(this.employee);
+    }  
+
+    /**
+     * Returns all Employee roles.
+     * 
+     * @return String 
+     */
+    public String getEmployeeRoles() {
+        // TODO: implement this method
+        return null;
+    }
+}
---
+++ src/main/java/app/ui/console/CreateEmployeeUI.java
@@ -0,0 +1,11 @@
+package app.ui.console;
+
+/**
+ * Register Employee View
+ * 
+ * @author Tomás Russo <1211288@isep.ipp.pt>
+ */
+
+public class CreateEmployeeUI {
+    
+}
