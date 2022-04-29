cfb5de7bf3eefc0b9c00334d52c6e74dbe0a0f0e
Tomás Russo <tomas.russo4@gmail.com>
2022-04-29 12:17:56 +0100

    feat(us-10): added EmployeeTest

---
+++ src/test/java/app/domain/model/EmployeeTest.java
@@ -0,0 +1,22 @@
+package app.domain.model;
+
+import org.junit.Test;
+import pt.isep.lei.esoft.auth.AuthFacade;
+import org.junit.Before;
+
+/**
+ * Employee's Tests
+ * 
+ * @author Tomás Russo <1211288@isep.ipp.pt>
+ */
+
+public class EmployeeTest {
+    AuthFacade authFacade;
+
+    @Before
+    public void setUp() {
+        this.authFacade = new AuthFacade();
+    }
+
+    // TODO: tests
+}
