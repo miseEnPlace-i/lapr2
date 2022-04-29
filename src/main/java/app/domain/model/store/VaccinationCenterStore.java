d9ece6e7e13285533f86014a896132c323e39aeb
AndreBarros1 <1211299@isep.ipp.pt>
2022-04-29 10:51:10 +0100

    docs(US09): updated VaccinationCenterStore

--- src/main/java/app/domain/model/store/VaccinationCenterStore.java
+++ src/main/java/app/domain/model/store/VaccinationCenterStore.java
@@ -2,7 +2,7 @@ package app.domain.model.store;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import app.domain.model.Employee;
 import app.domain.model.VaccinationCenter;
 
 public class VaccinationCenterStore {
@@ -14,18 +14,18 @@ public class VaccinationCenterStore {
         this.vacCenters = new ArrayList<VaccinationCenter>();
     }
 
-    public VaccinationCenter createVaccinationCenter(String name, String address, String emailAddress, int phoneNum,
-            int faxNum,
-            String webAddress, String openingHours, String closingHours, int slotDuration, int maxVacSlot,
-            String coordinator) {
+    public VaccinationCenter createVaccinationCenter(String name, String address,
+            String emailAddress, int phoneNum, int faxNum, String webAddress, String openingHours,
+            String closingHours, int slotDuration, int maxVacSlot, Employee coordinator) {
 
         // TO DO
         try {
         } catch (Exception e) {
         }
 
-        VaccinationCenter center = new VaccinationCenter(name, address, emailAddress, phoneNum, faxNum, webAddress,
-                openingHours, closingHours, slotDuration, maxVacSlot, coordinator);
+        VaccinationCenter center =
+                new VaccinationCenter(name, address, emailAddress, phoneNum, faxNum, webAddress,
+                        openingHours, closingHours, slotDuration, maxVacSlot, coordinator);
         return center;
     }
 
@@ -40,5 +40,21 @@ public class VaccinationCenterStore {
         if (vacCenters.contains(center)) {
             throw new IllegalArgumentException("Duplicate Vaccination Center");
         }
+    }
+
+    public void saveVaccinationCenter(VaccinationCenter center) {
+        String name = center.getName();
+        String address = center.getAddress();
+        String email = center.getEmailAddress();
+        int phoneNum = center.getPhoneNum();
+        int faxNum = center.getFaxNum();
+        String website = center.getWebAddress();
+        String openHours = center.getOpeningHours();
+        String cloHours = center.getClosingHours();
+        int slotDuration = center.getSlotDuration();
+        int maxVacSlot = center.getMaxVacSlot();
+        Employee coordinator = center.getCoordinator();
+
+
     }
 }
