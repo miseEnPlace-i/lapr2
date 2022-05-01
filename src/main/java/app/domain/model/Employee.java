141c3d36d251ce638cf14a041638beb9134327c4
Tomás Russo <tomas.russo4@gmail.com>
2022-05-01 17:06:21 +0100

    feat(us-10): Employee - created new methods, updated CCNumber type

--- src/main/java/app/domain/model/Employee.java
+++ src/main/java/app/domain/model/Employee.java
@@ -11,7 +11,7 @@ public class Employee {
   String phoneNumber = "";
   String email = "";
   String address = "";
-  String citizenCard = "";
+  int citizenCard = 0;
   String roleId = "";
 
   /**
@@ -24,13 +24,19 @@ public class Employee {
    * @param citizenCard the employee citizenCard
    * @param roleId the employee roleId
    */
-  public Employee(String name, String phoneNumber, String email, String address, String citizenCard,
+  public Employee(String name, String phoneNumber, String email, String address, int citizenCard,
       String roleId) {
     this.id = 123456789;
 
     // TODO
   }
 
+  /**
+   * Checks if the employee has the given role.
+   * 
+   * @param roleId the employee roleId
+   * @return true if the employee has the given role, false otherwise
+   */
   public boolean hasRoleId(String roleId) {
     return roleId.equals(this.roleId);
   }
@@ -45,5 +51,68 @@ public class Employee {
   public String toString() {
     // TODO
     return "";
+  }
+
+  /**
+   * Gets the employee id.
+   * 
+   * @return the employee id
+   */
+  public int getId() {
+    return id;  
+  }
+
+  /**
+   * Gets the employee name.
+   * 
+   * @return the employee name
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * Gets the employee phoneNumber.
+   * 
+   * @return the employee phoneNumber
+   */
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  /**
+   * Gets the employee email.
+   * 
+   * @return the employee email
+   */
+  public String getEmail() {
+    return email;
+  }
+
+  /**
+   * Gets the employee address.
+   * 
+   * @return the employee address
+   */
+  public String getAddress() {
+    return address;
+  }
+
+  /**
+   * Gets the employee citizenCard.
+   * 
+   * @return the employee citizenCard
+   */
+  public int getCitizenCard() {
+    return citizenCard;
+  }
+
+  /**
+   * Gets the employee roleId.
+   * 
+   * @return the employee roleId
+   */
+  public String getRoleId() {
+    return roleId;
   }
 }
