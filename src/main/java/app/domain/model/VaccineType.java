7688bf14b4156ac089d4d80e6bee3de32b73124b
Tomás Lopes <tomas050302@gmail.com>
2022-05-03 16:31:18 +0100

    feat(US12): added vaccine type params validations

--- src/main/java/app/domain/model/VaccineType.java
+++ src/main/java/app/domain/model/VaccineType.java
@@ -1,12 +1,42 @@
 package app.domain.model;
 
-/**
-* @author Carlos Lopes <1211277@isep.ipp.pt>
-*/
-public class VaccineType {
+import app.domain.shared.Constants;
 
-    private String code;
-    private String description;
-    private String technology;
-    
+/**
+ * @author Carlos Lopes <1211277@isep.ipp.pt>
+ * @author Tomás Lopes <1211289@isep.ipp.pt>
+ */
+public class VaccineType {
+  private String code;
+  private String description;
+  private String technology;
+
+  public VaccineType(String code, String description, String technology) {
+    setCode(code);
+    setDescription(description);
+    setTechnology(technology);
+  }
+
+  private void setCode(String code) {
+    if (code.length() != Constants.VACCINE_TYPE_CODE_LENGTH) throw new IllegalArgumentException("Vaccine type code must be " + Constants.VACCINE_TYPE_CODE_LENGTH + " characters long.");
+
+    this.code = code;
+  }
+
+  private void setDescription(String description) {
+    this.description = description;
+  }
+
+  private void setTechnology(String technology) {
+    if (!isVaccineTechnologyValid(technology)) throw new IllegalArgumentException("Invalid vaccine technology: " + technology);
+
+    this.technology = technology;
+  }
+
+  private boolean isVaccineTechnologyValid(String technology) {
+    for (Constants.VaccineTechnology vaccineTechnology : Constants.VaccineTechnology.values())
+      if (vaccineTechnology.toString().equals(technology)) return true;
+
+    return false;
+  }
 }
--- src/main/java/app/domain/model/store/VaccineTypeStore.java
+++ src/main/java/app/domain/model/store/VaccineTypeStore.java
@@ -6,20 +6,28 @@ import app.domain.model.VaccineType;
 
 
 /**
-* @author Carlos Lopes <1211277@isep.ipp.pt>
-*/
+ * @author Carlos Lopes <1211277@isep.ipp.pt>
+ * @author Tomás Lopes <1211289@isep.ipp.pt>
+ */
 public class VaccineTypeStore {
-    
-    private List<VaccineType> vaccineTypes;
+  private List<VaccineType> vaccineTypes;
 
-    
-    public VaccineTypeStore(){
-        this.vaccineTypes = new ArrayList<VaccineType>();
-    }
+  public VaccineTypeStore() {
+    this.vaccineTypes = new ArrayList<VaccineType>();
+  }
 
-    public VaccineType getVacTypeById(String vacTypeId){
-        //TODO
-        return null;
-    }
+  public VaccineType getVaccineTypeById(String vaccineTypeId) {
+    // TODO
+    return null;
+  }
 
+  public VaccineType addVaccineType(String code, String description, String technology) {
+    VaccineType vaccineType = new VaccineType(code, description, technology);
+
+    return vaccineType;
+  }
+
+  public boolean saveVaccineType(VaccineType vt) {
+    return vaccineTypes.add(vt);
+  }
 }
