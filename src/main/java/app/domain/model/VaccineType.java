c42389a0d12f4d153b4c4a47127c2db1ed23b04e
carlos <1211277@isep.ipp.pt>
2022-05-05 22:57:49 +0100

    feat(US13): implemented VaccineType Class

--- src/main/java/app/domain/model/VaccineType.java
+++ src/main/java/app/domain/model/VaccineType.java
@@ -8,5 +8,31 @@ public class VaccineType {
     private String code;
     private String description;
     private String technology;
+
+    public String getCode() {
+        return this.code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTechnology() {
+        return this.technology;
+    }
+
+    public void setTechnology(String technology) {
+        this.technology = technology;
+    }
+
+
     
 }
