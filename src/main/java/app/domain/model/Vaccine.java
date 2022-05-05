01c63cf50f178f32e4c1a4a5268151c131e40a9e
carlos <1211277@isep.ipp.pt>
2022-05-05 22:57:22 +0100

    feat(US13): implemented Vaccine Class

--- src/main/java/app/domain/model/Vaccine.java
+++ src/main/java/app/domain/model/Vaccine.java
@@ -12,18 +12,53 @@ public class Vaccine {
     private String brand;
     private String id;
     private VaccineType vacType;
+
+    public String getDesignation() {
+        return this.designation;
+    }
+
+    public void setDesignation(String designation) {
+        validateDesignation(designation);
+        this.designation = designation;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public void setBrand(String brand) {
+        validateBrand(brand);
+        this.brand = brand;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        validateId(id);
+        this.id = id;
+    }
+
+    public VaccineType getVacType() {
+        return this.vacType;
+    }
+
+    public void setVacType(VaccineType vacType){
+        this.vacType = vacType;
+    }
+
     private AdminProcList adminProcList;
 
-    public Vaccine(String designation, String id, String brand, String vacTypeId){
+    public Vaccine(String designation, String id, String brand, VaccineType vacType){
         validateDesignation(designation);
         validateBrand(brand);
         validateId(id);
-        validateVacType(vacTypeId);
 
         this.designation = designation;
         this.brand = brand;
         this.id = id;
-        this.vacType = VaccineTypeStore.getVacTypeById(vacTypeId);\
+        this.vacType = vacType;
     }
 
     public void validateDesignation(String designation){
@@ -44,20 +79,18 @@ public class Vaccine {
         }
     }
 
-    public void validateVacType(String vacTypeId){
-        if(vacTypeId == "" || vacTypeId == null){
-            throw new IllegalArgumentException("Vaccine type id field is required.");
-        }
-
-        if(VaccineTypeStore.getVacTypeById(vacTypeId)==null){
+    public void validateVacType(VaccineType vacType){
+        if(vacType == null){
             throw new IllegalArgumentException("The vaccine type must exist.");
         }
     }
 
 
     public void createAdminProc(int minAge, int maxAge, int numberOfDoses){
-        //TODO
+        AdminProcess ap = new AdminProcess(minAge, maxAge, numberOfDoses);
+        adminProcList.addAdminProc(ap);
     }
 
     
+
 }
