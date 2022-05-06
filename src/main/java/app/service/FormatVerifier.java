4810a93cd3313f1dd50b800d3252aab3d85a360c
unknown <1211299@isep.ipp.pt>
2022-05-06 23:28:57 +0100

    feat: added validate methods

--- src/main/java/app/service/FormatVerifier.java
+++ src/main/java/app/service/FormatVerifier.java
@@ -8,30 +8,42 @@ public class FormatVerifier {
     }
 
     public static boolean validateEmail(String email) {
-        if (!email.matches("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")) {
+        if (email.matches("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")) {
             return true;
         } else {
             return false;
         }
     }
 
-    public static boolean validatePhoneNumber() {
-        // TODO
-        return false;
+    public static boolean validatePhoneNumber(String number) {
+        if (number.matches("\\+[0-9]{3}[0-9]{9}$")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public static boolean validateFaxNumber() {
-        // TODO
-        return false;
+    public static boolean validateFaxNumber(String number) {
+        if (number.matches("\\+[0-9]{1,3}-[0-9]{3}\\-[0-9]{7}")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public static boolean validateWebAddress() {
-        // TODO
-        return false;
+    public static boolean validateWebAddress(String web) {
+        if (web.matches("^(https:\\/\\/|https:\\/\\/)?(www.)?([a-zA-Z0-9]+).[a-zA-Z0-9]*.[a-z]{3}.?([a-z]+)?$")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public static boolean validateSNSNumber() {
-        // TODO
-        return false;
+    public static boolean validateSNSNumber(String number) {
+        if (number.matches("^\\d{9}$")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
