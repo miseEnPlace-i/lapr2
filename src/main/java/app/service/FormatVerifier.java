dd831cc3507e5c2be75f80734b0c5f37404f8d14
unknown <1211299@isep.ipp.pt>
2022-05-06 22:45:04 +0100

    feat: added validateEmail method

--- src/main/java/app/service/FormatVerifier.java
+++ src/main/java/app/service/FormatVerifier.java
@@ -7,9 +7,12 @@ public class FormatVerifier {
         return ccVerifier.validate(cc);
     }
 
-    public static boolean validateEmail() {
-        // TODO
-        return false;
+    public static boolean validateEmail(String email) {
+        if (!email.matches("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static boolean validatePhoneNumber() {
