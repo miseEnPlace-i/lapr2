8f5c35557c3317e4101304d7ad19d8308cb6dca0
unknown <1211299@isep.ipp.pt>
2022-05-08 11:34:54 +0100

    chore(US09): indentation, added new javadoc

--- src/test/java/app/domain/model/store/VaccinationCenterStoreTest.java
+++ src/test/java/app/domain/model/store/VaccinationCenterStoreTest.java
@@ -10,60 +10,74 @@ import app.domain.shared.Constants;
  * @author André Barros <1211299@isep.ipp.pt>
  */
 public class VaccinationCenterStoreTest {
-        VaccinationCenterStore store = new VaccinationCenterStore();
-        Employee coordinator;
-        VaccinationCenter center;
+  VaccinationCenterStore store = new VaccinationCenterStore();
+  Employee coordinator;
+  VaccinationCenter center;
 
-        @Before
-        public void setUp() {
-                coordinator = new Employee("Joana", "+351916478865", "email@email.com", "address",
-                                "000000000ZZ4", Constants.ROLE_COORDINATOR);
-        }
+  /**
+   * Set up for the tests
+   */
+  @Before
+  public void setUp() {
+    coordinator = new Employee("Joana", "+351916478865", "email@email.com", "address",
+        "000000000ZZ4", Constants.ROLE_COORDINATOR);
+  }
 
-        @Test
-        public void ensureAddCenterIsWorkingCorrectly() {
-                center = store.createVaccinationCenter("name", "address", "email@email.com",
-                                "+351961919169", "+351961919169", "http://www.google.com", "10:00",
-                                "19:00", 5, 5, this.coordinator);
+  /**
+   * Check that saveVaccinationCenter is working properly
+   * 
+   * @throws IllegalArgumentException
+   */
+  @Test
+  public void ensureAddCenterIsWorkingCorrectly() {
+    center = store.createVaccinationCenter("name", "address", "email@email.com", "+351961919169",
+        "+351961919169", "http://www.google.com", "10:00", "19:00", 5, 5, this.coordinator);
 
-                assert store.size() == 0;
+    assert store.size() == 0;
 
-                store.saveVaccinationCenter(center);
+    store.saveVaccinationCenter(center);
 
-                assert store.size() == 1;
-        }
+    assert store.size() == 1;
+  }
 
-        /**
-         * Check that checkDuplicates method is checking duplicated centers properly
-         * 
-         * @throws IllegalArgumentException
-         */
-        @Test(expected = IllegalArgumentException.class)
-        public void ensureCheckDuplicatesIsWorkingCorrectly() {
-                store.validateVaccinationCenter(center);
-        }
+  /**
+   * Check that checkDuplicates method is checking duplicated centers properly
+   * 
+   * @throws IllegalArgumentException
+   */
+  @Test(expected = IllegalArgumentException.class)
+  public void ensureCheckDuplicatesIsWorkingCorrectly() {
+    store.validateVaccinationCenter(center);
+  }
 
-        @Test(expected = IllegalArgumentException.class)
-        public void ensureValidateIsWorking() {
-                store.validateVaccinationCenter(null);
-        }
+  /**
+   * Check that validateVaccinationCenter is working properly
+   * 
+   * @throws IllegalArgumentException
+   */
+  @Test(expected = IllegalArgumentException.class)
+  public void ensureValidateIsWorking() {
+    store.validateVaccinationCenter(null);
+  }
 
-        @Test
-        public void ensureIsPossibleToAddAnotherCenter() {
-                assert store.size() == 0;
+  /**
+   * Check that it is possible to add centers to the system
+   */
+  @Test
+  public void ensureIsPossibleToAddAnotherCenter() {
+    assert store.size() == 0;
 
-                store.saveVaccinationCenter(center);
+    store.saveVaccinationCenter(center);
 
-                assert store.size() == 1;
+    assert store.size() == 1;
 
-                center = store.createVaccinationCenter("name123", "address", "email@email.com",
-                                "+351961919169", "+351961919169", "http://www.google.com", "10:00",
-                                "19:00", 5, 5, this.coordinator);
+    center = store.createVaccinationCenter("name123", "address", "email@email.com", "+351961919169",
+        "+351961919169", "http://www.google.com", "10:00", "19:00", 5, 5, this.coordinator);
 
-                store.validateVaccinationCenter(center);
+    store.validateVaccinationCenter(center);
 
-                store.saveVaccinationCenter(center);
+    store.saveVaccinationCenter(center);
 
-                assert store.size() == 2;
-        }
+    assert store.size() == 2;
+  }
 }
