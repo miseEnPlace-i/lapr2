cf0810bd387bea3bf4bf9d4c6d9a07160310c6d8
Ricardo <rmoreiradev@gmail.com>
2022-05-13 13:40:57 +0100

    feat(US04): added methods to find an appointment

--- src/main/java/app/domain/model/Appointment.java
+++ src/main/java/app/domain/model/Appointment.java
@@ -2,12 +2,44 @@ package app.domain.model;
 
 import java.util.Calendar;
 
+/**
+ * Appointment class.
+ * 
+ * @author Ricardo Moreira <1211285@isep.ipp.pt>
+ */
 public class Appointment {
     private int snsNumber;
     private Calendar date;
 
+    //// an appointment should have a vaccine type if it is in a healthcare center
+    // TODO: and a center where it is being administered
+
+    private VaccinationCenter center;
+
+    /**
+     * Constructor for Appointment.
+     * 
+     * @param snsNumber The SNS number of the user this appointment is related to.
+     * @param date The date of the appointment.
+     */
     public Appointment(int snsNumber, Calendar date) {
         this.snsNumber = snsNumber;
         this.date = date;
+    }
+
+    /**
+     * Compares the SNS number of this appointment with another number.
+     */
+    public boolean hasSnsNumber(int snsNumber) {
+        return this.snsNumber == snsNumber;
+    }
+
+    /**
+     * Checks if the date of this appointment is in the next hour.
+     */
+    public boolean isInTheNextHour() {
+        Calendar now = Calendar.getInstance();
+        now.add(Calendar.HOUR, 1);
+        return this.date.after(now);
     }
 }
---
+++ src/main/java/app/domain/model/store/AppointmentStore.java
@@ -0,0 +1,44 @@
+package app.domain.model.store;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import app.domain.model.Appointment;
+
+/**
+ * AppointmentStore class.
+ * 
+ * @author Ricardo Moreira <1211285@isep.ipp.pt>
+ */
+public class AppointmentStore {
+    private List<Appointment> appointments;
+
+    /**
+     * Constructor for AppointmentStore.
+     */
+    public AppointmentStore() {
+        this.appointments = new ArrayList<Appointment>();
+    }
+
+    /**
+     * Creates a new appointment.
+     */
+    public Appointment addAppointment(int snsNumber, Calendar date) {
+        // TODO: validations once more in the attack
+        Appointment appointment = new Appointment(snsNumber, date);
+        appointments.add(appointment);
+        return appointment;
+    }
+
+    /**
+     * Finds an appointment by its SNS number.
+     */
+    public Appointment findAppointment(int snsNumber) {
+        for (Appointment appointment : appointments) {
+            // TODO: check center
+            if (appointment.hasSnsNumber(snsNumber) && appointment.isInTheNextHour())
+                return appointment;
+        }
+        return null;
+    }
+}
