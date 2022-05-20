47f14e9761daf3ef2b03c961a4fea27ccfc6b29a
unknown <1211299@isep.ipp.pt>
2022-05-20 23:36:18 +0100

    feat: updated vaccination center controller (added constructor to community mass and health care)

--- src/main/java/app/controller/RegisterVaccinationCenterController.java
+++ src/main/java/app/controller/RegisterVaccinationCenterController.java
@@ -29,7 +29,7 @@ public class RegisterVaccinationCenterController implements IRegisterController 
   }
 
   /**
-   * Creates an Vaccination Center instance and validates it.
+   * Creates a Community Mass Vaccination Center instance and validates it.
    * 
    * @param name the vaccination center name
    * @param address the vaccination center address
@@ -43,9 +43,38 @@ public class RegisterVaccinationCenterController implements IRegisterController 
    * @param maxVacSlot the vaccination center maximum vaccines per slot
    * @param coordinator the vaccination center coordinator
    */
-  public void create(String name, String address, String emailAddress, String phoneNum,
+  public void createCommunityMass(String name, String address, String emailAddress, String phoneNum,
       String faxNum, String webAddress, String openingHours, String closingHours, int slotDuration,
       int maxVacSlot, Employee coordinator) {
+
+    // creates a vaccination center instance
+    this.center = vacStore.createVaccinationCenter(name, address, emailAddress, phoneNum, faxNum,
+        webAddress, openingHours, closingHours, slotDuration, maxVacSlot, coordinator);
+
+    // validates the center
+    vacStore.validateVaccinationCenter(center);
+  }
+
+  /**
+   * Creates a Health Care Center instance and validates it.
+   * 
+   * @param name the vaccination center name
+   * @param address the vaccination center address
+   * @param emailAddress the vaccination center email address
+   * @param phoneNum the vaccination center phone number
+   * @param faxNum the vaccination center fax number
+   * @param webAddress the vaccination center website address
+   * @param openingHours the vaccination center opening hours
+   * @param closingHours the vaccination center closing hours
+   * @param slotDuration the vaccination center slot duration
+   * @param maxVacSlot the vaccination center maximum vaccines per slot
+   * @param coordinator the vaccination center coordinator
+   * @param ages the vaccination center AGES
+   * @param ars the vaccination center ARS
+   */
+  public void createHealthCare(String name, String address, String emailAddress, String phoneNum,
+      String faxNum, String webAddress, String openingHours, String closingHours, int slotDuration,
+      int maxVacSlot, Employee coordinator, String ages, String ars) {
 
     // creates a vaccination center instance
     this.center = vacStore.createVaccinationCenter(name, address, emailAddress, phoneNum, faxNum,
