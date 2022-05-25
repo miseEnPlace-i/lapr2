af02fada86d958b97ea81c02293cd9bedae376de
Ricardo <rmoreiradev@gmail.com>
2022-05-25 12:24:27 +0100

    feat: added VaccinationHistoryList

---
+++ src/main/java/app/domain/model/list/VaccinationHistoryList.java
@@ -0,0 +1,13 @@
+package app.domain.model.list;
+
+public class VaccinationHistoryList {
+    // private List<Vaccine> vaccinationHistory;
+
+    /**
+     * Constructor for the WaitingRoomList class.
+     */
+    public VaccinationHistoryList() {
+
+    }
+
+}
