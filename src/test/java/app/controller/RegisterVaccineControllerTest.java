b9f3013bf10d8bcc46b652995597663e7fa80419
AndreBarros1 <1211299@isep.ipp.pt>
2022-05-28 23:44:58 +0100

    tests: added Vaccine controller tests

---
+++ src/test/java/app/controller/RegisterVaccineControllerTest.java
@@ -0,0 +1,72 @@
+package app.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import app.domain.model.AdminProcess;
+import app.domain.model.Company;
+import app.domain.model.Vaccine;
+import app.domain.model.VaccineType;
+import app.domain.model.list.AdminProcList;
+import app.domain.model.store.VaccineStore;
+import app.domain.model.store.VaccineTypeStore;
+
+public class RegisterVaccineControllerTest {
+  Company company = new Company("designation", "12345");
+  RegisterVaccineController controller = new RegisterVaccineController(company);
+
+  private VaccineStore vaccineStore;
+  private VaccineTypeStore vaccineTypeStore;
+  private VaccineType vaccineType;
+  private AdminProcList list;
+  private String resourceName;
+  private Vaccine vaccine;
+  private AdminProcess process;
+
+  @Before
+  public void setUp() {
+    vaccineStore = company.getVaccineStore();
+
+    vaccineTypeStore = company.getVaccineTypeStore();
+    vaccineType = new VaccineType("11111", "test", "test_technology");
+    vaccineTypeStore.saveVaccineType(vaccineType);
+
+    resourceName = "Vaccine";
+
+    vaccine = new Vaccine("teste", "123123", "brand", vaccineType);
+
+    list = new AdminProcList();
+
+  }
+
+
+
+  @Test
+  public void ensureGetResourceNameIsWorkingCorrectly() {
+    assertEquals(controller.getResourceName(), resourceName);
+  }
+
+  /**
+   * Check that it is possible to create a vaccine
+   */
+  @Test
+  public void ensureItIsPossibleToCreateVaccine() {
+    controller.createVaccine("test", "00000", "Test", "11111");
+    controller.save();
+
+    assertEquals(vaccineStore.size(), 1);
+
+  }
+
+  /**
+   * Check that list of vaccine types is working
+   */
+  @Test
+  public void ensureListVaccineTypesWorks() {
+    List<VaccineType> list = controller.getVacTypes();
+    assertEquals(list.size(), 1);
+    assertEquals(list.get(0), vaccineType);
+  }
+
+}
