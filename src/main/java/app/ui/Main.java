40422b16ad5033c07bf6ad2646e85f420383561b
Ricardo <rmoreiradev@gmail.com>
2022-06-02 10:32:30 +0100

    feat: added initial gui

--- src/main/java/app/ui/Main.java
+++ src/main/java/app/ui/Main.java
@@ -1,6 +1,7 @@
 package app.ui;
 
 import app.ui.console.MainMenuUI;
+import app.ui.gui.App;
 
 /**
  *
@@ -12,8 +13,8 @@ public class Main {
   public static void main(String[] args) {
     try {
       MainMenuUI menu = new MainMenuUI();
-
-      menu.run();
+      
+      menu.run();      
     } catch (Exception e) {
       e.printStackTrace();
     }
---
+++ src/main/java/app/ui/gui/MainMenuUI.java
@@ -0,0 +1,19 @@
+package app.ui.gui;
+
+import javafx.fxml.Initializable;
+
+public class MainMenuUI implements Initializable {    
+    private App mainApp;
+
+    @Override
+    public void initialize(java.net.URL location, java.util.ResourceBundle resources) {
+        
+    }
+
+    public void setMainApp(App mainApp) {
+        this.mainApp = mainApp;
+    }
+
+
+
+}
