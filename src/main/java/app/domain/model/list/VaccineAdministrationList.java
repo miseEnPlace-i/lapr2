2f60cb082d675a2b001ced1b548fa62300fed20c
Tomás Russo <tomas.russo4@gmail.com>
2022-06-08 11:46:02 +0100

    feat(US08): updated VaccineAdministrationList

--- src/main/java/app/domain/model/list/VaccineAdministrationList.java
+++ src/main/java/app/domain/model/list/VaccineAdministrationList.java
@@ -4,10 +4,15 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
+import app.dto.AdverseReactionDTO;
+import app.dto.VaccineDTO;
+import app.domain.model.AdverseReaction;
 import app.domain.model.SNSUser;
 import app.domain.model.VaccinationCenter;
 import app.domain.model.Vaccine;
 import app.domain.model.VaccineType;
+import app.mapper.AdverseReactionMapper;
+import app.mapper.VaccineMapper;
 import app.domain.model.VaccineAdministration;
 
 public class VaccineAdministrationList {
@@ -38,13 +43,81 @@ public class VaccineAdministrationList {
   }
 
   /**
+   * Validates a vaccine administration object.
+   * 
+   * @param vaccineAdministration the vaccine administration object to be validated
+   */
+  public void validateVaccineAdministration(VaccineAdministration vaccineAdministration) {
+
+  }
+
+  /**
    * Saves a vaccine administration object.
    * 
    * @param vaccineAdministration the vaccine administration object to be saved
    */
   public void saveVaccineAdministration(VaccineAdministration vaccineAdministration) {
     vaccineAdministrations.add(vaccineAdministration);
+
+    VaccinationCenter vaccinationCenter = vaccineAdministration.getVaccinationCenter();
+
+    vaccinationCenter.addVaccineAdministrationToList(vaccineAdministration);
+
+    // vaccineAdministration.setSMSSending();
   }
 
+  /**
+   * Gets the list of adverse reactions.
+   * 
+   * @return a list of AdverseReactionDTO
+   */
+  public List<AdverseReactionDTO> getAdverseReactions() {
+    List<AdverseReaction> adverseReactions = new ArrayList<>();
 
+    for (VaccineAdministration vaccineAdministration : vaccineAdministrations) {
+      if (vaccineAdministration.getAdverseReaction() != null) {
+        adverseReactions.add(vaccineAdministration.getAdverseReaction());
+      }
+    }
+
+    return AdverseReactionMapper.toDto(adverseReactions);
+  }
+
+  /**
+   * Get the last taken vaccine of a given vaccine type by a user.
+   * 
+   * @param VaccineType the vaccine type
+   * 
+   * @return the last taken vaccine of a given vaccine type by a user
+   */
+  public VaccineDTO getLastTakenVaccineByVaccineType(VaccineType vaccineType) {
+    Collections.sort(vaccineAdministrations);
+
+    for (VaccineAdministration vaccineAdministration : vaccineAdministrations) {
+      if (vaccineAdministration.vaccineHasVaccineType(vaccineType)) {
+        return VaccineMapper.toDto(vaccineAdministration.getVaccine());
+      }
+    }
+
+    return null;
+  }
+
+  /**
+   * Get the next dose number to administer to a user.
+   * 
+   * @param Vaccine the vaccine to check the next dose number
+   * 
+   * @return the next dose number to administer to a user
+   */
+  public int getNextDoseNumberOfVaccine(Vaccine vaccine) {
+    Collections.sort(vaccineAdministrations);
+
+    for (VaccineAdministration vaccineAdministration : vaccineAdministrations) {
+      if (vaccineAdministration.getVaccine().equals(vaccine)) {
+        return vaccineAdministration.getDoseNumber() + 1;
+      }
+    }
+
+    return 1;
+  }
 }
