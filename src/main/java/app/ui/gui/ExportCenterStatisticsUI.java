6dbdf2e6c94b8346386ecf60202f042f2962b2ce
unknown <1211299@isep.ipp.pt>
2022-06-09 21:56:00 +0100

    feat: updated export statistics UI (label of center name fixed)

--- src/main/java/app/ui/gui/ExportCenterStatisticsUI.java
+++ src/main/java/app/ui/gui/ExportCenterStatisticsUI.java
@@ -1,11 +1,30 @@
 package app.ui.gui;
 
+import app.controller.App;
 import app.controller.ExportCenterStatisticsController;
+import app.controller.FindCoordinatorVaccinationCenterController;
+import app.domain.model.Company;
+import app.session.EmployeeSession;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 
 public class ExportCenterStatisticsUI extends ChildUI<CoordinatorUI> {
     private ExportCenterStatisticsController ctrl;
+    private EmployeeSession employeeSession;
+    private FindCoordinatorVaccinationCenterController ctrlCenter;
+
+    @FXML
+    private Label lblCenterName;
 
     void init() {
-        // this.ctrl = new ExportCenterStatisticsController();
+        App app = App.getInstance();
+        Company company = app.getCompany();
+        this.employeeSession = new EmployeeSession();
+        this.ctrlCenter = new FindCoordinatorVaccinationCenterController(company, employeeSession);
+
+        this.ctrlCenter.findCoordinatorCenter();
+
+        this.lblCenterName.setText(this.ctrlCenter.getVaccinationCenterName());
     }
 }
