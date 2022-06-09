fc0ea641f9575d4c4266df554e2d5db994681e02
unknown <1211299@isep.ipp.pt>
2022-06-09 18:11:50 +0100

    feat: added new UI to export and added action to btn in GUI

---
+++ src/main/java/app/ui/gui/ExportCenterStatisticsUI.java
@@ -0,0 +1,11 @@
+package app.ui.gui;
+
+import app.controller.ExportCenterStatisticsController;
+
+public class ExportCenterStatisticsUI extends ChildUI<CoordinatorUI> {
+    private ExportCenterStatisticsController ctrl;
+
+    void init() {
+        // this.ctrl = new ExportCenterStatisticsController();
+    }
+}
