56f60cc17c5a5bade6d12db292b818a8540efde5
Ricardo <rmoreiradev@gmail.com>
2022-06-17 11:06:19 +0100

    feat: added email

--- src/main/java/app/domain/shared/Constants.java
+++ src/main/java/app/domain/shared/Constants.java
@@ -35,6 +35,7 @@ public class Constants {
   public static final byte VACCINE_TYPE_CODE_LENGTH = 5;
 
   public static final String SMS_FILE = "SMS.txt";
+  public static final String EMAIL_FILE = "Email.txt";
 
   public static final int MAX_OF_PASSWORD_TRIES = 3;
 
