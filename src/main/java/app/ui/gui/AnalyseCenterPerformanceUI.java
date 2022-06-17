bf1dcfb745e11ddfccc8737ccfc3582372b77bcc
Tomás Lopes <tomas050302@gmail.com>
2022-06-17 13:56:49 +0100

    style(analyseCenterResult): updated style

--- src/main/java/app/ui/gui/AnalyseCenterPerformanceUI.java
+++ src/main/java/app/ui/gui/AnalyseCenterPerformanceUI.java
@@ -21,11 +21,13 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -49,13 +51,22 @@ public class AnalyseCenterPerformanceUI extends ChildUI<CoordinatorUI> {
 
   public void setEmployeeSession(EmployeeSession session) {
     this.employeeSession = session;
-    this.ctrl = new AnalyseCenterPerformanceController(App.getInstance().getCompany(), employeeSession);
+    this.ctrl = new AnalyseCenterPerformanceController(
+        App.getInstance().getCompany(), employeeSession);
   }
 
   @FXML
   void checkAnalyseButtonEnable() {
-    if (dtpDate.getValue() != null && !StringUtils.isEmpty(txtInterval.getText())) btnAnalyse.setDisable(false);
+    if (isValidInput()) btnAnalyse.setDisable(false);
     else btnAnalyse.setDisable(true);
+  }
+
+  private boolean isValidInput() {
+    if (dtpDate.getValue() != null
+        && !StringUtils.isEmpty(txtInterval.getText()))
+      return true;
+    else
+      return false;
   }
 
   @FXML
@@ -65,7 +76,8 @@ public class AnalyseCenterPerformanceUI extends ChildUI<CoordinatorUI> {
 
   @FXML
   void handleAnalyse() {
-    analyse();
+    if (isValidInput()) analyse();
+    else Utils.showError("Non valid input!", "Please fill all inputs.");
   }
 
   private void analyse() {
@@ -75,10 +87,12 @@ public class AnalyseCenterPerformanceUI extends ChildUI<CoordinatorUI> {
     int interval = Integer.parseInt(txtInterval.getText());
 
     try {
-      CenterPerformance performance = ctrl.analyseCenterPerformance(day, interval);
+      CenterPerformance performance =
+          ctrl.analyseCenterPerformance(day, interval);
 
       if (performance == null) {
-        Utils.showError("No performance data found", "No performance data found for the selected date.");
+        Utils.showError("No performance data found",
+            "No performance data found for the selected date.");
         resetFields();
         return;
       }
@@ -94,33 +108,35 @@ public class AnalyseCenterPerformanceUI extends ChildUI<CoordinatorUI> {
     txtInterval.setText("");
   }
 
-  private FlowPane generatePaneWithData(String title, String data) {
-    Label lblInputList = new Label(title);
-    Label lblList = new Label(data);
-    lblList.setAlignment(Pos.CENTER_LEFT);
+  private FlowPane generatePaneWithData(String title, String data,
+      double width) {
+    final double padding = 80;
 
-    ScrollPane container = new ScrollPane(lblList);
-    container.setHbarPolicy(ScrollBarPolicy.AS_NEEDED);
-    container.setVbarPolicy(ScrollBarPolicy.NEVER);
-    container.setPadding(new Insets(8, 8, 8, 8));
-    container.setPrefWidth(560);
-    container.setPrefHeight(48);
+    Label lblInputList = new Label(title);
+    lblInputList.setTranslateX(padding + 2);
+
+    TextArea container = new TextArea(data);
+    container.setFont(new Font("System", 17));
+    container.setEditable(false);
+    container.setWrapText(true);
+    container.setPrefWidth(width - padding * 2);
+    container.setTranslateX(padding);
     container.setMaxWidth(Double.MAX_VALUE);
 
     FlowPane inputListContainer = new FlowPane(lblInputList, container);
     inputListContainer.setOrientation(Orientation.HORIZONTAL);
     inputListContainer.setHgap(8);
     inputListContainer.setVgap(8);
+    inputListContainer.setAlignment(Pos.CENTER_LEFT);
     inputListContainer.setMaxWidth(Double.MAX_VALUE);
-    inputListContainer.setAlignment(Pos.CENTER);
     inputListContainer.setPadding(new Insets(8, 8, 8, 8));
 
     return inputListContainer;
   }
 
   private void loadDialog(CenterPerformance performance) {
-    final double SCENE_WIDTH = 640.0;
-    final double SCENE_HEIGHT = 400.0;
+    final double SCENE_WIDTH = 720.0;
+    final double SCENE_HEIGHT = 480.0;
 
     final Stage dialog = new Stage();
     dialog.initModality(Modality.APPLICATION_MODAL);
@@ -131,25 +147,35 @@ public class AnalyseCenterPerformanceUI extends ChildUI<CoordinatorUI> {
     dialog.setHeight(SCENE_HEIGHT);
 
     Properties props = PropertiesUtils.getProperties();
-    String algorithmUsed = props.getProperty(Constants.PARAMS_PERFORMANCE_ALGORITHM);
+    String algorithmUsed =
+        props.getProperty(Constants.PARAMS_PERFORMANCE_ALGORITHM);
 
-    FlowPane inputListContainer = generatePaneWithData("Input List", performance.stringifyDifferencesList());
-    FlowPane maxSubListContainer = generatePaneWithData("Max Sum Sublist", performance.stringifyMaxSumSublist());
-    FlowPane sumContainer = generatePaneWithData("Max Sum", "" + performance.getMaxSum());
-    FlowPane begginingIntervalContainer = generatePaneWithData("Beggining Time of Interval", performance.getStartingInterval() + "h");
-    FlowPane endIntervalContainer = generatePaneWithData("End Time of Interval", performance.getEndingInterval() + "h");
-    FlowPane algorithmUsedContainer = generatePaneWithData("Algorithm Used", algorithmUsed);
-    FlowPane timeElapsedContainer = generatePaneWithData("Time Elapsed", performance.getTimeElapsed() + " ms");
+    FlowPane inputListContainer = generatePaneWithData("Input List",
+        performance.stringifyDifferencesList(), SCENE_WIDTH);
+    FlowPane maxSubListContainer = generatePaneWithData("Max Sum Sublist",
+        performance.stringifyMaxSumSublist(), SCENE_WIDTH);
+    FlowPane sumContainer = generatePaneWithData("Max Sum",
+        "" + performance.getMaxSum(), SCENE_WIDTH);
+    FlowPane beginningIntervalContainer =
+        generatePaneWithData("Beggining Time of Interval",
+            performance.getStartingInterval() + "h", SCENE_WIDTH);
+    FlowPane endIntervalContainer = generatePaneWithData("End Time of Interval",
+        performance.getEndingInterval() + "h", SCENE_WIDTH);
+    FlowPane algorithmUsedContainer =
+        generatePaneWithData("Algorithm Used", algorithmUsed, SCENE_WIDTH);
+    FlowPane timeElapsedContainer = generatePaneWithData("Time Elapsed",
+        performance.getTimeElapsed() + " ms", SCENE_WIDTH);
 
-    VBox pane = new VBox(15);
+    VBox pane = new VBox(16);
 
     // Setting the space between the nodes of a VBox pane
-    pane.setPadding(new Insets(40, 40, 40, 40));
     pane.setAlignment(Pos.CENTER);
-    pane.getChildren().addAll(inputListContainer, maxSubListContainer, sumContainer, begginingIntervalContainer, endIntervalContainer, algorithmUsedContainer,
-        timeElapsedContainer);
+    pane.getChildren().addAll(inputListContainer, maxSubListContainer,
+        sumContainer, beginningIntervalContainer, endIntervalContainer,
+        algorithmUsedContainer, timeElapsedContainer);
 
     ScrollPane container = new ScrollPane(pane);
+    container.setPadding(new Insets(16, 0, 32, 0));
 
     container.setHbarPolicy(ScrollBarPolicy.NEVER);
 
