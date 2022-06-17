761768eb72762811d88705420ea0dfc29d199410
Tomás Lopes <tomas050302@gmail.com>
2022-06-17 16:01:32 +0100

    feat: added chart

--- src/main/java/app/ui/gui/AnalyseCenterPerformanceUI.java
+++ src/main/java/app/ui/gui/AnalyseCenterPerformanceUI.java
@@ -2,6 +2,7 @@ package app.ui.gui;
 
 import java.sql.Date;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Properties;
 import org.apache.commons.lang3.StringUtils;
 import app.controller.AnalyseCenterPerformanceController;
@@ -11,11 +12,16 @@ import app.domain.shared.Constants;
 import app.service.PropertiesUtils;
 import app.session.EmployeeSession;
 import app.ui.gui.utils.Utils;
+import app.utils.Time;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
@@ -34,6 +40,8 @@ import javafx.stage.Stage;
 public class AnalyseCenterPerformanceUI extends ChildUI<CoordinatorUI> {
   private AnalyseCenterPerformanceController ctrl;
   private EmployeeSession employeeSession;
+
+  private int interval;
 
   @FXML
   private Button btnAnalyse;
@@ -84,7 +92,7 @@ public class AnalyseCenterPerformanceUI extends ChildUI<CoordinatorUI> {
     Calendar day = Calendar.getInstance();
     day.setTime(Date.valueOf(dtpDate.getValue()));
 
-    int interval = Integer.parseInt(txtInterval.getText());
+    interval = Integer.parseInt(txtInterval.getText());
 
     try {
       CenterPerformance performance =
@@ -108,31 +116,6 @@ public class AnalyseCenterPerformanceUI extends ChildUI<CoordinatorUI> {
     txtInterval.setText("");
   }
 
-  private FlowPane generatePaneWithData(String title, String data,
-      double width) {
-    final double padding = 80;
-
-    Label lblInputList = new Label(title);
-    lblInputList.setTranslateX(padding + 2);
-
-    TextArea container = new TextArea(data);
-    container.setFont(new Font("System", 17));
-    container.setEditable(false);
-    container.setWrapText(true);
-    container.setPrefWidth(width - padding * 2);
-    container.setTranslateX(padding);
-    container.setMaxWidth(Double.MAX_VALUE);
-
-    FlowPane inputListContainer = new FlowPane(lblInputList, container);
-    inputListContainer.setOrientation(Orientation.HORIZONTAL);
-    inputListContainer.setHgap(8);
-    inputListContainer.setVgap(8);
-    inputListContainer.setAlignment(Pos.CENTER_LEFT);
-    inputListContainer.setMaxWidth(Double.MAX_VALUE);
-    inputListContainer.setPadding(new Insets(8, 8, 8, 8));
-
-    return inputListContainer;
-  }
 
   private void loadDialog(CenterPerformance performance) {
     final double SCENE_WIDTH = 720.0;
@@ -166,13 +149,18 @@ public class AnalyseCenterPerformanceUI extends ChildUI<CoordinatorUI> {
     FlowPane timeElapsedContainer = generatePaneWithData("Time Elapsed",
         performance.getTimeElapsed() + " ms", SCENE_WIDTH);
 
+    BarChart chart = generateChart(performance);
+    chart.setPrefWidth(SCENE_WIDTH - 80);
+    chart.setBarGap(0);
+    chart.setCategoryGap(0);
+
     VBox pane = new VBox(16);
 
     // Setting the space between the nodes of a VBox pane
     pane.setAlignment(Pos.CENTER);
     pane.getChildren().addAll(inputListContainer, maxSubListContainer,
         sumContainer, beginningIntervalContainer, endIntervalContainer,
-        algorithmUsedContainer, timeElapsedContainer);
+        algorithmUsedContainer, timeElapsedContainer, chart);
 
     ScrollPane container = new ScrollPane(pane);
     container.setPadding(new Insets(16, 0, 32, 0));
@@ -180,8 +168,71 @@ public class AnalyseCenterPerformanceUI extends ChildUI<CoordinatorUI> {
     container.setHbarPolicy(ScrollBarPolicy.NEVER);
 
     Scene scene = new Scene(container, SCENE_WIDTH, SCENE_HEIGHT);
+    scene.getStylesheets().add(getClass()
+        .getResource("/styles/AnalyseCenterResults.css").toExternalForm());
     dialog.setResizable(false);
     dialog.setScene(scene);
     dialog.show();
+  }
+
+  private BarChart<String, Number> generateChart(
+      CenterPerformance performance) {
+    CategoryAxis xAxis = new CategoryAxis();
+    NumberAxis yAxis = new NumberAxis();
+
+    XYChart.Series<String, Number> series = new XYChart.Series<>();
+    XYChart.Series<String, Number> worstSeries = new XYChart.Series<>();
+
+    List<Integer> differences = performance.getDifferencesList();
+    Time startingInterval = performance.getStartingInterval();
+    Time endingInterval = performance.getEndingInterval();
+    Time tempInterval = startingInterval.clone();
+
+    for (Integer value : differences) {
+      if (tempInterval.isBetween(startingInterval, endingInterval))
+        worstSeries.getData().add(
+            new XYChart.Data<String, Number>(tempInterval.toString(), value));
+      else
+        series.getData().add(
+            new XYChart.Data<String, Number>(tempInterval.toString(), value));
+
+      tempInterval.addMinutes(interval);
+    }
+
+    BarChart<String, Number> chart = new BarChart<>(xAxis, yAxis);
+
+    chart.getData().addAll(worstSeries, series);
+
+    worstSeries.setName("Bad Performance");
+    series.setName("Good Performance");
+
+
+    return chart;
+  }
+
+  private FlowPane generatePaneWithData(String title, String data,
+      double width) {
+    final double padding = 80;
+
+    Label lblInputList = new Label(title);
+    lblInputList.setTranslateX(padding + 2);
+
+    TextArea container = new TextArea(data);
+    container.setFont(new Font("System", 17));
+    container.setEditable(false);
+    container.setWrapText(true);
+    container.setPrefWidth(width - padding * 2);
+    container.setTranslateX(padding);
+    container.setMaxWidth(Double.MAX_VALUE);
+
+    FlowPane inputListContainer = new FlowPane(lblInputList, container);
+    inputListContainer.setOrientation(Orientation.HORIZONTAL);
+    inputListContainer.setHgap(8);
+    inputListContainer.setVgap(8);
+    inputListContainer.setAlignment(Pos.CENTER_LEFT);
+    inputListContainer.setMaxWidth(Double.MAX_VALUE);
+    inputListContainer.setPadding(new Insets(8, 8, 8, 8));
+
+    return inputListContainer;
   }
 }
