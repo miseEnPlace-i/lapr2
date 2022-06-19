3beec996f5425a9c52994064551ecbf5ff3a7ac2
carlos <1211277@isep.ipp.pt>
2022-06-19 16:10:20 +0100

    merge: conflicts

---
+++ src/main/java/app/domain/model/Scheduler.java
@@ -0,0 +1,23 @@
+package app.domain.model;
+
+import java.util.Calendar;
+import java.util.Timer;
+import app.domain.model.store.VaccinationCenterStore;
+import app.domain.model.store.VaccineTypeStore;
+import app.utils.Time;
+
+/**
+ * @author Carlos Lopes <1211277@isep.ipp.pt>
+ */
+public class Scheduler {
+
+  private final static int SECOND_INTERVAL = 60*60*24;
+
+  public static void scheduleExportDailyVaccinated(String path, String time, String separator, VaccinationCenterStore vacCenterStore, VaccineTypeStore vacTStore){
+        
+
+      
+
+    }
+    
+}
